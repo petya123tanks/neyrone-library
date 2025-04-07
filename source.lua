17730ef27c34a33544373e30ff781d990af2adbe
petya123tanks <alexpopovich110377@gmail.com>
2025-04-07 22:14:15 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -641,7 +641,7 @@ function library:init()
                             option.values = value == nil and '' or value
                         end
                         if type(option.checkconfig) == "boolean" then
-                            print"yes"
+                            --print"yes"
                             option.checkconfig = true
                         end
                         if option.refresh then
