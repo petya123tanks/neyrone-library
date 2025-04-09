e8e42c05543095595b4abb689e4843f2a03baf5f
petya123tanks <alexpopovich110377@gmail.com>
2025-04-09 17:46:02 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1221,6 +1221,7 @@ function library:init()
         local window = {
             title = data.title or '',
             selectedTab = nil;
+            huy = data.huy or false;
             openyaica = data.openyaica or true;
             tabs = {},
             objects = {},
@@ -1344,7 +1345,7 @@ function library:init()
             })
 
             objs.columnholder1 = utility:Draw('Square', {
-                Size = newUDim2(.48, 0, .96, 0);
+                Size = window.huy and newUDim2(.9, 0, .96, 0) or newUDim2(.48, 0, .96, 0);
                 Position = newUDim2(.01, 0, .02, 0);
                 Transparency = 0;
                 ZIndex = z+6;
@@ -1352,7 +1353,7 @@ function library:init()
             })
 
             objs.columnholder2 = utility:Draw('Square', {
-                Size = newUDim2(.48, 0, .96, 0);
+                Size = window.huy and newUDim2(.9, 0, .96, 0) or newUDim2(.48, 0, .96, 0);
                 Position = newUDim2(1 - (.48 + .01), 0, .02, 0);
                 Transparency = 0;
                 ZIndex = z+6;
