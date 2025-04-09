9d99f8bbc5b80d2c664066bed24293ae03f42bad
petya123tanks <alexpopovich110377@gmail.com>
2025-04-09 15:57:47 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1926,30 +1926,17 @@ function library:init()
                         self.objects.background.Visible = bool;
                     end
                 end)
-                --[[else потом, позже
-                    self.open = window.openyaica;
-                    local objs = self.objects.background:GetDescendants()
-                    table.insert(objs, self.objects.background)
-                    task.spawn(function()
-                        if not window.openyaica then
-                            task.wait(.1);
-                        end
-                        self.objects.background.Visible = window.openyaica;
-                    end)
-                end--]]
+
                 for _,v in next, objs do
                     if v.Object.Transparency ~= 0 then
                         task.spawn(function()
                             if window.openyaica == false or window.openyaica == false and bool then
                                 utility:Tween(v.Object, 'Transparency', visValues[v] or 1, .1);
-                                print("debil")
                             else
                                 if bool then
-                                    --v.Object.Transparency = 1
                                     utility:Tween(v.Object, 'Transparency', visValues[v] or 1, .1);
                                 else
                                     visValues[v] = v.Object.Transparency;
-                                    --v.Object.Transparency = 0
                                     utility:Tween(v.Object, 'Transparency', .05, .1);
                                 end
                             end
