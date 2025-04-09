2194010464d20bf0fd5015a60972abedf8b86e6f
petya123tanks <alexpopovich110377@gmail.com>
2025-04-09 20:11:41 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -718,7 +718,6 @@ function library:init()
     end
 
     for i,v in next, self.images do
-        deletefile(self.cheatname..'/assets/'..i..'.oh')
         if not isfile(self.cheatname..'/assets/'..i..'.oh') then
             writefile(self.cheatname..'/assets/'..i..'.oh', game:HttpGet(v))
         end
