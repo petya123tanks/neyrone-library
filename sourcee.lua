a46797de5db3b331abe3d87cc113e7ab4149f0f1
petya123tanks <alexpopovich110377@gmail.com>
2025-04-20 03:23:38 +0700

    Update sourcee.lua

--- sourcee.lua
+++ sourcee.lua
@@ -1207,7 +1207,7 @@ function library:init()
 
             objs.imagebackground = utility:Draw('Image', {
                 Size = size;
-                Position = newUDim2(0, 450, 0, 150);
+                Position = newUDim2(0, 500, 0, 170);
                 Data = library.images.blackstartv;
                 ThemeColor = 'Accent';
                 ZIndex = z+1;
